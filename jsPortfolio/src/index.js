d0ff872aa0ae6dff86f3e634cae0989694094808
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-03-18 01:35:15 -0300

    :white_check_mark: Clase N°6: Babel Loader para JavaScript

---
+++ jsPortfolio/src/index.js
@@ -0,0 +1,9 @@
+import Template from './templates/Template.js';
+
+console.log('hola');
+
+(async function App () {
+	const main = null || document.getElementById('main');
+
+	main.innerHTML = await Template();
+})();
