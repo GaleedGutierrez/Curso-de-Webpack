6cc7e4084d48c6792e97c6864709c3549eacb42b
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-03-30 22:21:10 -0300

    :white_check_mark: Clase N°11: Loaders de fuentes

--- jsPortfolio/webpack.config.js
+++ jsPortfolio/webpack.config.js
@@ -4,8 +4,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const output = {
-	path     : path.resolve(__dirname, 'dist'),
-	filename : 'main.js'
+	path                : path.resolve(__dirname, 'dist'),
+	filename            : 'main.js',
+	assetModuleFilename : 'assets/images/[hash][ext][query]'
 };
 
 const rulesJavascript = {
@@ -28,6 +29,30 @@ const rulesCSS = {
 const rulesAssets = {
 	test : /\.png$/,
 	type : 'asset/resource'
+};
+
+// webpack 4
+// const rulesFonts = {
+// 	test : /\.(woff|woff2)$/,
+// 	use  : {
+// 		loader  : 'url-loader',
+// 		options : {
+// 			limit      : 10_000,
+// 			mimetype   : 'application/font-woff',
+// 			name       : '[name].[ext]',
+// 			outputPath : './assets/fonts/',
+// 			publicPath : './assets/fonts/',
+// 			esModule   : false,
+// 		}
+// 	}
+// };
+
+const rulesFonts = {
+	test      : /\.(woff|woff2|eot|ttf|otf)$/i,
+	type      : 'asset/resource',
+	generator : {
+		filename : 'assets/fonts/[hash][ext][query]',
+	},
 };
 
 const plugins = [
@@ -56,6 +81,7 @@ module.exports = {
 			rulesJavascript,
 			rulesCSS,
 			rulesAssets,
+			rulesFonts
 		]
 	},
 	plugins
